379f300c3bca9861f63b3ddeeb47baa532a2eaa6
sw <sw@swdemacbook-pro.local>
2016-01-05 19:09:57 +0800

    初始化项目

---
+++ githubDemo/githubDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubDemo
+//
+//  Created by sw on 16/1/5.
+//  Copyright © 2016年 sw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubDemo/githubDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubDemo
+//
+//  Created by sw on 16/1/5.
+//  Copyright © 2016年 sw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
